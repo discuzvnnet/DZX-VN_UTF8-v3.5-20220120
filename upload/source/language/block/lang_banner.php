dff6f9f86a6d2a921ebc87809264695a0e480633
discuzvn <discuzvnnet@gmail.com>
2022-01-21 13:55:11 +0700

    translated

--- upload/source/language/block/lang_banner.php
+++ upload/source/language/block/lang_banner.php
@@ -13,15 +13,15 @@ if(!defined('IN_DISCUZ')) {
 
 $lang = array
 (
-	'banner_pic' => '图片地址',
-	'banner_width' => '图片宽度',
-	'banner_height' => '图片高度',
-	'banner_url' => '图片链接',
-	'banner_text' => '图片介绍',
-	'banner_atarget' => '链接打开方式',
-	'banner_atarget_top' => '本页面打开(跳出框架)',
-	'banner_atarget_blank' => '新窗口打开',
-	'banner_atarget_self' => '本页面打开',
+	'banner_pic' => 'Link ảnh',
+	'banner_width' => 'Chiều rộng ảnh',
+	'banner_height' => 'Chiều dài ảnh',
+	'banner_url' => 'Link quảng cáo',
+	'banner_text' => 'Mô tả hình ảnh',
+	'banner_atarget' => 'Mở nhiều liên kết',
+	'banner_atarget_top' => 'Mở trong cửa sổ này (_top)',
+	'banner_atarget_blank' => 'Mở trong một cửa sổ mới',
+	'banner_atarget_self' => 'Mở trong cửa sổ này',
 );
 
 ?>
